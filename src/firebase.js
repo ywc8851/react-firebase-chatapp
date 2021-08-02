24f3dab20db3165c04c95dc0b3abb21599b0d9c2
ywc8851 <60125497+ywc8851@users.noreply.github.com>
2021-08-02 21:03:22 +0900

    Day1
    create-react-app , firebase연동

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firbase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+var firebaseConfig = {
+  apiKey: "AIzaSyD8V6Y6Gm_2lsNYPC-GwO6dr8TSbPgH55U",
+  authDomain: "chat-app-13d3f.firebaseapp.com",
+  projectId: "chat-app-13d3f",
+  storageBucket: "chat-app-13d3f.appspot.com",
+  messagingSenderId: "481255318319",
+  appId: "1:481255318319:web:92b265941b1245b9a477da",
+  measurementId: "G-9ETHLP00HG",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
